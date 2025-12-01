988c00a382f7136c77ef190fbdd419c99998e557
kz <pilkarz4a@gmail.com>
2025-12-01 20:50:54 +0100

    zmiana maila w commitach bo byl zly...

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Route::get('/dashboard', [TaskController::class, 'board'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
